33632ce0350ef32a5fc121895765172979ccb527
Randerson Lemos <randerson.lemos@gmail.com>
2022-10-28 10:58:28 -0300

    Dataset inclusion and initialization of data augumentation

--- source.sh
+++ source.sh
@@ -1,6 +1,6 @@
-source ~/anaconda3/bin/activate mc866-pf
+source ~/anaconda3/bin/activate mc886-pf
 
-LD_LIBRARY_PATH="$HOME/anaconda3/pkgs/cudatoolkit-11.2.2-hbe64b41_10/lib"
-LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$HOME/anaconda3/pkgs/cudnn-8.1.0.77-h90431f1_0/lib"
+#LD_LIBRARY_PATH="$HOME/anaconda3/pkgs/cudatoolkit-11.2.2-hbe64b41_10/lib"
+#LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$HOME/anaconda3/pkgs/cudnn-8.1.0.77-h90431f1_0/lib"
 
 #echo $LD_LIBRARY_PATH
